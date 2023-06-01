9787eba09de97a25544aa9bba6ccb080a750fdef
MichaelMantovani <michmantovani@gmail.com>
2023-06-01 16:19:30 +0200

    Collect elements from DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log('JS OK')
+
+
+// Recupero gli elementi dal DOM
+const gallery = document.getElementById('gallery')
+const next = document.getElementById('next')
+const prev = document.getElementById('prev')
