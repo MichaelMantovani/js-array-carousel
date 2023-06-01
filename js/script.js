67bb1ed0a513f267a20db1733463a721024a8435
MichaelMantovani <michmantovani@gmail.com>
2023-06-01 16:33:43 +0200

    Add images in DOM

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@ console.log('JS OK')
 
 
 // Recupero gli elementi dal DOM
-const gallery = document.getElementById('gallery')
-const next = document.getElementById('next')
-const prev = document.getElementById('prev')
+const gallery = document.getElementById('gallery');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+const image = document.querySelector('#carousel img');
+
+// Creo un array con le immagini 
+const imagesGallery = ['img/01.webp' , 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp'];
+
+
+// Inserisco le immagini nel DOM
+let imagesElement = ''
+
+for (i = 0; i < imagesGallery.length; i++) {
+  imagesElement += `<img src="${imagesGallery[i]}" alt="Pictures ${i + 1}">`
+}
+
+gallery.innerHTML = imagesElement
+
